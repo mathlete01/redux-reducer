d9ebf2324eb8716b019a3e5b45c809b3f7316ac3
mathlete01 <msallin@gmail.com>
2021-01-12 21:19:42 -0800

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,19 @@
-export function manageFriends(state, action){
+const state = {
+  friends: [],
+};
+
+export function manageFriends(state, action) {
+  switch (action.type) {
+    case "ADD_FRIEND":
+      return {
+        ...state,
+        friends: [...state.friends, action.friend],
+      };
+    case "REMOVE_FRIEND":
+      return {
+        friends: state.friends.filter((el) => el.id !== action.id),
+      };
+    default:
+      return state;
+  }
 }
--- src/reducers/managePresents.js
+++ src/reducers/managePresents.js
@@ -1,2 +1,13 @@
-export function managePresents(state, action){
+// class ManagePresents extends Component {
+const state = {
+  numberOfPresents: 0,
+};
+
+export function managePresents(state, action) {
+  switch (action.type) {
+    case "INCREASE":
+      return { numberOfPresents: state.numberOfPresents + 1 };
+    default: 
+      return state
+  }
 }
